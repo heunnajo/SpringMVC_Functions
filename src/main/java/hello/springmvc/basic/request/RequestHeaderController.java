818e1ee284b8bb79f18b3059fa36616e5ad2cd25
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-09 11:22:37 +0900

    Get Various data type of parameter from HTTP request.
    1. HttpServletRequest
    2. HttpServletResponse
    3. HttpMethod
    4. Locale
    5. @RequestHeader MultiValueMap<String,String>
    6 @RequestHeader("host")
    7. @CookieValue(value = "myCookie", required=false)

---
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,36 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+    @RequestMapping("/headers")
+    //다양한 정보를 파라미터로 받을 수 있다.locale : 언어 정보
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String,String> headerMap,
+                          @RequestHeader("host") String aa,
+                          @CookieValue(value = "myCookie", required=false) String cookie){//required=false : 없어도 됨.
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", aa);
+        log.info("myCookie={}", cookie);
+        return "ok";
+    }
+}
