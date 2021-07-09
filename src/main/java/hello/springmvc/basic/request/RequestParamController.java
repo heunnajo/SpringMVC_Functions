fdb68511ae433b7b415a85faa12ed82ce85d4a52
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-09 11:47:43 +0900

    Get Parameter data from HTTP request.

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,22 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class RequestParamController {
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+        log.info("username={}, age={}",username,age);
+
+        res.getWriter().write("ok");
+    }
+}
